74e8586ac4aa114a853f5593fefa553eb70025d7
rogding <rogding@users.noreply.github.com>
2018-09-06 21:47:50 -0500

    Dropped rows without measured energies

--- data/daz_1995/daz_1995-analysis.py
+++ data/daz_1995/daz_1995-analysis.py
@@ -48,6 +48,9 @@ for i in DATA_FILES:
     df_temp['E_exp'] = df_temp['Obs']
     df_temp['E_exp_unc'] = 0.2
     
+    # Drop rows states without an assigned energy
+    df_temp.dropna(subset=['E_exp'], inplace=True)   
+    
     df_output = pd.concat([df_output, df_temp], join='inner', ignore_index=True)
 
 
